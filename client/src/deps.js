905ee733ebb605ec426a24e6b68dee0ee22920f2
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-08-21 15:24:16 +1200

    add deps

---
+++ client/src/deps.js
@@ -0,0 +1,3 @@
+export {GUI} from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/libs/dat.gui.module.js';
+export * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js';
+// import "https://cdn.jsdelivr.net/npm/socket.io@4.1.3/client-dist/socket.io.js"
