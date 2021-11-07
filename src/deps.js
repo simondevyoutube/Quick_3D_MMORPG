d33418a16f41bd8fe66491eeb1a685a3890b4641
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-11-07 21:59:25 +1300

    deno disabled

--- src/deps.js
+++ src/deps.js
@@ -1,14 +1,14 @@
 // export * as tweakpane from "https://cdn.skypack.dev/tweakpane";
 // TODO-DefinitelyMaybe: import only the required things from three
 // export * as matter from 'https://cdn.skypack.dev/matter-js';
-export * as cannon from "https://cdn.skypack.dev/cannon-es";
-export * as THREE from "https://cdn.skypack.dev/three";
-export { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
-export { LineMaterial } from "https://cdn.skypack.dev/three/examples/jsm/lines/LineMaterial.js";
-export { Wireframe } from "https://cdn.skypack.dev/three/examples/jsm/lines/Wireframe.js";
-export { WireframeGeometry2 } from "https://cdn.skypack.dev/three/examples/jsm/lines/WireframeGeometry2.js";
-export { FBXLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/FBXLoader.js";
-export { GLTFLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js";
-export { OBJLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/OBJLoader.js";
-export { MTLLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/MTLLoader.js";
-export { clone as deepClone } from "https://cdn.skypack.dev/three/examples/jsm/utils/SkeletonUtils.js";
+export * as cannon from "cannon-es";
+export * as THREE from "three";
+export { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+export { LineMaterial } from "three/examples/jsm/lines/LineMaterial.js";
+export { Wireframe } from "three/examples/jsm/lines/Wireframe.js";
+export { WireframeGeometry2 } from "three/examples/jsm/lines/WireframeGeometry2.js";
+export { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
+export { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+export { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+export { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+export { clone as deepClone } from "three/examples/jsm/utils/SkeletonUtils.js";
