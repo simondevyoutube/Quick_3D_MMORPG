8649fb42da498e4596c373b8887d3434cdb451e8
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-07-27 11:39:49 +1200

    begin update

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
